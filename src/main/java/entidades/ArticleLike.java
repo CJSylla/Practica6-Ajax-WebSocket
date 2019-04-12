9a1dbd620e2e82f25ad6fa71fc42c2f8eb051e73
Claude Junior Sylla <cjsylla93@gmail.com>
2019-04-12 07:25:57 -0400

    Entidades

---
+++ src/main/java/entidades/ArticleLike.java
@@ -0,0 +1,50 @@
+package entidades;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+
+@Entity
+@NamedQueries({@NamedQuery(name = "ArticleLike.findAllByArticulo", query = "select l from ArticleLike l where l.articulo like :articulo")})
+public class ArticleLike implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @OneToOne
+    private Usuario autor;
+
+    @ManyToOne
+    private Articulo articulo;
+
+    public ArticleLike( Usuario autor,Articulo articulo) {
+        this.autor = autor;
+        this.articulo =articulo;
+    }
+    public ArticleLike(){
+
+    }
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Articulo getArticulo() {
+        return articulo;
+    }
+
+    public void setArticulo(Articulo articulo) {
+        this.articulo = articulo;
+    }
+
+    public Usuario getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Usuario autor) {
+        this.autor = autor;
+    }
+}
+
---
+++ src/main/java/entidades/Dislike.java
@@ -0,0 +1,50 @@
+package entidades;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+
+@Entity
+@NamedQueries({@NamedQuery(name = "Dislike.findAllByComentario", query = "select l from Dislike l where l.comentario like :comentario")})
+public class Dislike implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @OneToOne
+    private Usuario autor;
+
+    @ManyToOne
+    private Comentario comentario;
+
+    public Dislike( Usuario autor,Comentario comentario) {
+        this.autor = autor;
+        this.comentario=comentario;
+    }
+    public Dislike(){
+
+    }
+
+    public Comentario getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(Comentario comentario) {
+        this.comentario = comentario;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Usuario getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Usuario autor) {
+        this.autor = autor;
+    }
+}
